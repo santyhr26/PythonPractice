41fe90f8eeeb98aa48eb2627eb94cf0839521604
santyhr26 <santosh212379394@gmail.com>
2024-08-21 12:04:18 -0400

    first commit

---
+++ .venv/test.py
@@ -0,0 +1,7 @@
+print('Program to sum 2 numbers')
+Num1 = int(input('Enter the first number'))
+Num2 = int(input('Enter the second number'))
+
+Result = Num1 + Num2
+
+print(f'Sum of two numbers are {Result}')
